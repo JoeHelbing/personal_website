43f2c89fa28910e99607a051d73520baf77298f9
Joe Helbing <joehelbing@gmail.com>
2024-09-29 01:09:19 -0500

    another try

--- app.py
+++ app.py
@@ -9,9 +9,4 @@ def get():
         P("Welcome to my GitHub Pages site powered by FastHTML.")
     )
 
-if __name__ == "__main__":
-    content = get()
-    if isinstance(content, tuple):
-        content = ''.join(str(item) for item in content)
-    print(content)
-
+serve()
